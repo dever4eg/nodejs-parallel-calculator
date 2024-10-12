885890a0fccec2e96f6c8e7bd51b75e0c091d36d
Bohdan Onyshchenko <bohdan.onyshchenko@guesty.com>
2024-10-12 23:15:53 +0200

    Use evaluation service in controller

--- src/evaluation/controllers/evaluation.controller.ts
+++ src/evaluation/controllers/evaluation.controller.ts
@@ -1,15 +1,17 @@
 import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { EvaluationRequest } from './evaluation.request';
-import { calculateMath } from '../math/calculate-math';
+import { EvaluationService } from '../evaluation.service';
 
 @Controller()
 export class EvaluationController {
+  constructor(private readonly evaluationService: EvaluationService) {}
+
   @Post('/evaluate')
   @HttpCode(200)
   async evaluate(@Body() evaluateRequest: EvaluationRequest) {
     const { expression } = evaluateRequest;
 
-    const result = await calculateMath(expression);
+    const result = await this.evaluationService.evaluate(expression);
 
     return {
       result,
